2fabc4bc0b20b207bdcffbc8f36d5e5d7b3b7a7c
Mohammad Nour Rezek <Eng.M_Nour@outlook.com>
2020-12-17 00:53:23 +0200

    add prompt, confirm

--- javascript/add-content.js
+++ javascript/add-content.js
@@ -1,6 +1,7 @@
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
+
 if (hourNow > 18) {
     greeting = 'Good evening, Class!';
   } else if (hourNow > 12) {
@@ -10,5 +11,9 @@ if (hourNow > 18) {
   } else {
     greeting = 'Something went wrong!';
   }
-  alert(greeting) ;
-document.write('<h3>'+greeting+'</h3>');
+
+  prompt("What's your name?");
+  
+  confirm("Are you sure?") ;
+
+  document.write('<h3>'+greeting+'</h3>');
